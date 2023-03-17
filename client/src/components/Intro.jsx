98269a9549930dddb2daff22d236f9bc75203b94
Leftmostchain21 <thomassey476@outlook.com>
2023-03-17 13:51:31 +0000

    Added a favicon

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -6,20 +6,19 @@ function Intro() {
     <div className="App">
       {/* create a home page for a football website  */}
       <header className="App-header">
-      <body>
+        <body>
+          <main>
+            <h2>Welcome to Fantasy Football</h2>
+            <p>Create your team, join a league and compete with other users!</p>
+          </main>
 
-    <main>
-      <h2>Welcome to Fantasy Football</h2>
-      <p>Create your team, join a league and compete with other users!</p>
-    </main>
-
-    <footer>
-      <p>&copy; 2022 Fantasy Football</p>
-    </footer>
-  </body>
+          <footer>
+            <p>&copy; 2022 Fantasy Football</p>
+          </footer>
+        </body>
       </header>
     </div>
-  )
+  );
 }
 
 export default Intro
