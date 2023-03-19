102897058af06c226951fc83c4625bdc1ca93cef
RC101003 <roycheung1010@gmail.com>
2023-03-19 17:10:27 +0000

    init fix nav bar

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require ('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,54 +17,9 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfully");
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-// const router = express.Router();
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-
-// const User = require('../models/User');
-
-// router.post('/login', async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Check if user with email exists
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(401).json({ message: 'Invalid email or password' });
-//     }
-
-//     // Check if password is correct
-//     const passwordMatch = await bcrypt.compare(password, user.password);
-//     if (!passwordMatch) {
-//       return res.status(401).json({ message: 'Invalid email or password' });
-//     }
-
-//     // Create and sign a JWT token
-//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-//     // Return the user data and token
-//     res.json({ user: { name: user.name, email: user.email }, token });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: 'Internal server error' });
-//   }
-// });
-
-// module.exports = router;
