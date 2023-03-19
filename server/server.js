e2bda2227da0f1df45fccce4da9cd46a9c476e35
RC101003 <roycheung1010@gmail.com>
2023-03-19 15:06:35 +0000

    init newserver

--- server/server.js
+++ server/server.js
@@ -1,35 +1,70 @@
 const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const User = require('../models/User');
+require ('dotenv').config();
 
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+const app = express();
+const port = process.env.PORT || 5000;
 
-    // Check if user with email exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
+app.use(cors());
+app.use(express.json());
 
-    // Check if password is correct
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
 
-    // Create and sign a JWT token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
 
-    // Return the user data and token
-    res.json({ user: { name: user.name, email: user.email }, token });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 });
 
-module.exports = router;
+
+
+
+
+
+
+
+
+
+
+// const router = express.Router();
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+
+// const User = require('../models/User');
+
+// router.post('/login', async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     // Check if user with email exists
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(401).json({ message: 'Invalid email or password' });
+//     }
+
+//     // Check if password is correct
+//     const passwordMatch = await bcrypt.compare(password, user.password);
+//     if (!passwordMatch) {
+//       return res.status(401).json({ message: 'Invalid email or password' });
+//     }
+
+//     // Create and sign a JWT token
+//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+//     // Return the user data and token
+//     res.json({ user: { name: user.name, email: user.email }, token });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// });
+
+// module.exports = router;
